b726f1a68e78f359dc6e8daaa91653343eb34913
tariqul islam <tariqul.isha@gmail.com>
2016-04-06 02:48:44 +0600

    Feature: Client model created

---
+++ models/client.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = function(sequelize , DataTypes){
+
+	var client = sequelize.define('client', {
+		mobile: { type: DataTypes.STRING, primaryKey: true},
+		password: {type: DataTypes.STRING, allowNull: false},
+		full_name: { type: DataTypes.STRING},
+		address: { type: DataTypes.STRING},
+		national_id: {type: DataTypes.STRING},
+		verification_code: {type: DataTypes.INTEGER},
+		national_id: {type: DataTypes.STRING},
+		status: {
+			type: DataTypes.ENUM('draft', 'verified'), 
+			defaultValue: 'draft',
+			allowNull: false
+		}
+	} ,  {
+
+		classMethods: {
+			associate: function(models){
+
+				client.belongsTo(models.region , { foreignKey: "regionId" });
+			
+			}
+		}
+		
+	});
+
+	return client;
+};
