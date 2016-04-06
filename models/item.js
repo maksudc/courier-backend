6f7e1e3957d86ac0a5a59a82f18640e9a620c96b
Md. Maksud Alam Chowdhury <mc65799@gmail.com>
2016-04-07 02:46:59 +0600

    adds tracker integration with order item

--- models/item.js
+++ models/item.js
@@ -12,6 +12,8 @@ var item = sequelize.define('item', {
 });
 
 module.exports = item;*/
+var Promise = require("bluebird");
+
 module.exports = function(sequelize , DataTypes){
 
 	var item = sequelize.define('item', {
@@ -24,10 +26,101 @@ module.exports = function(sequelize , DataTypes){
 		classMethods: {
 			associate: function(models){
 				item.belongsTo(models.products , { foreignKey: "productUuid" });
-				item.belongsTo(models.order , { foreignKey: "orderUuid" });
-				item.hasOne(models.genericTracker , { foreignKey: "trackableId" , constraints: false , scope:{ trackableType: "orderItem" } });
+				item.belongsTo(models.order , { foreignKey: "orderUuid" , as:"order" });
+				item.hasOne(models.genericTracker , {
+					foreignKey: "trackableId" ,
+					constraints: false ,
+					scope:{ trackableType: "orderItem" },
+					as: "tracker"
+				});
 			}
 		}
+	});
+
+	item.hook("afterCreate" , function(orderItem , options){
+		/**
+			* Create a tracker item corresponding to the order
+		**/
+		orderItem
+		.getTracker()
+		.then(function(currentTrackerItem){
+			if(!currentTrackerItem){
+
+				var trackerData = {};
+
+				trackerData.trackableType = "orderItem";
+				trackerData.trackableId = orderItem.uuid;
+
+				return sequelize.models.genericTracker.create(trackerData);
+
+			}else{
+
+				return Promise.resolve(currentTrackerItem);
+			}
+		})
+		.then(function(trackerItem){
+
+			var p1 = orderItem.getOrder();
+			var p2 = Promise.resolve(trackerItem);
+
+			return Promise.all([p1 , p2]);
+		})
+		.then(function(results){
+
+			console.log("Found result: "+ results);
+			var trackerData = {};
+
+			var orderInstance = results[0];
+			var trackerItem = results[1];
+
+			if(orderInstance.entry_branch_type == "regional-branch"){
+				trackerData.sourceBranchType = "regional";
+			}else{
+				trackerData.sourceBranchType = "sub";
+			}
+			trackerData.sourceBranchId = parseInt(orderInstance.entry_branch);
+
+			if(orderInstance.exit_branch_type == "regional-branch"){
+				trackerData.destinationBranchType = "regional";
+			}else{
+				trackerData.destinationBranchType = "sub";
+			}
+			trackerData.destinationBranchId = parseInt(orderInstance.exit_branch);
+
+			trackerData.currentBranchType = trackerData.sourceBranchType;
+			trackerData.currentBranchId = trackerData.sourceBranchId;
+
+			var p3 = orderInstance.getTracker();
+			var p4 = Promise.resolve(trackerItem);
+			var p5 = trackerItem.update(trackerData);
+
+			return Promise.all([p3 , p4 , p5]);
+		})
+		.then(function(results){
+
+			console.log("Found result: "+ results);
+
+			var parentTracker = results[0];
+			var trackerItem = results[1];
+			var updateResult = results[2];
+
+			if(parentTracker){
+				var p6 = trackerItem.update( { parentTrackerId: parentTracker.uuid } );
+				var p7 = parentTracker.update({
+					hasChild: true
+				});
+
+				return Promise.all([ p6 , p7 ]);
+			}else{
+				return Promise.reject("No tracker for order");
+			}
+		})
+		.then(function(results){
+			console.log(" Post Update parent tracker:  " + results);
+		})
+		.catch(function(err){
+			console.log(err);
+		});
 	});
 
 	return item;
