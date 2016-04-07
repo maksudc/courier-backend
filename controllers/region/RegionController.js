73ec0a840ed3f22e1df5c6255fbab695705536ac
tariqul islam <tariqul.isha@gmail.com>
2016-04-08 02:51:04 +0600

    Feature: Get all branches /region/getAll

--- controllers/region/RegionController.js
+++ controllers/region/RegionController.js
@@ -1,11 +1,21 @@
 var DB = require("../../models/index");
 var sequelize = DB.sequelize;
+var regionLogic = require("../../logics/regionLogic");
 
 var RegionController = function(){};
 
 RegionController.prototype.index = function(req , res){
 
     res.send({ "status": "In Region management page" });
+    
+};
+
+RegionController.prototype.getAll = function(req , res){
+
+	regionLogic.findAll(function(regionList){
+		res.send({ "status": "Get all the regions", data: regionList});
+	});
+    
 };
 
 module.exports = new RegionController();
