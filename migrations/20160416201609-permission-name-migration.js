9619ad149a179a9509886cbef0092c1584c7835f
tariqul islam <tariqul.isha@gmail.com>
2016-04-16 20:19:58 +0600

    Feature: Added permission name ot permission model

---
+++ migrations/20160416201609-permission-name-migration.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    queryInterface.addColumn('permissions', 'name', {
+      type: Sequelize.STRING, unique: true
+    });
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    queryInterface.removeColumn('permissions', 'name');
+  }
+};
--- models/permission.js
+++ models/permission.js
@@ -3,6 +3,7 @@
 module.exports = function(sequelize , DataTypes){
 
 	var permission = sequelize.define('permission', {
+		name: {type: DataTypes.STRING, unique: true},
 		url: { type: DataTypes.STRING, primaryKey: true},
 		description: {type: DataTypes.STRING, allowNull: false},
 		system_operator : { type: DataTypes.BOOLEAN, defaultValue: false},
