263fe883a46520e804c09daa2c83b08e2a9e0d30
Md. Maksud Alam Chowdhury <mc65799@gmail.com>
2016-04-21 02:13:34 +0600

    data population in tests

--- test/shipment.js
+++ test/shipment.js
@@ -3,26 +3,108 @@ var assert = require("chai").assert;
 var DB = require("../models/index");
 var sequelize = DB.sequelize;
 var Sequelise = DB.Sequelize;
+var fs = require("fs");
 
 var shipment = sequelize.models.shipment;
-sequelize.sync({ force:true });
+var regionalBranch = sequelize.models.regionalBranch;
+var subBranche = sequelize.models.subBranch;
+var route = sequelize.models.branchRoute;
+
+var Promise = require("bluebird");
 
 describe("Shipment Tests" , function(){
 
+  this.timeout(15000);
+
   describe('description', function(){
     // body...
-
     it("shipment creation" , function(done){
 
-      shipment
-      .findAll()
-      .then(function(rs){
-        console.log(rs);
-        assert.equal([].length , rs.length);
-        done();
+      var p1 = Promise.resolve(true);
+
+      if(process.env.NODE_ENV == 'test'){
+        p1 = sequelize.sync({ force:true });
+      }
+      // Create the branches
+      var branchDataContents = fs.readFileSync("./test/fixtures/routes/branchData.json");
+      var branchFixtures = JSON.parse(branchDataContents);
+
+      return p1
+      .then(function(){
+        return branchFixtures;
+      })
+      .map(function(branchFixture){
+
+        return regionalBranch.create({
+          label: branchFixture.label,
+          branchType: branchFixture.branchType
+        })
+        .then(function(rBranchItem){
+
+          return Promise.map(branchFixture.SubBranches , function(sBranchFixture){
+
+              return subBranche.create({
+                label: sBranchFixture.label,
+                branchType: sBranchFixture.branchType,
+                regionalBranchId: rBranchItem.id
+              });
+          });
+        });
+      })
+      .then(function(results){
+        // Add the Fuxture routes
+
+        var routeFixtureContent = fs.readFileSync("./test/fixtures/routes/routeData.json");
+        var routeFixtures = JSON.parse(routeFixtureContent);
+
+        return Promise.map(routeFixtures , function(routeFixture){
+
+          var sourceBranchId = null;
+          var destinationBranchId = null;
+
+          return regionalBranch
+          .findOne({ where: { label: routeFixture.sourceBranchLabel } })
+          .then(function(sourceRegionalBranch){
+            sourceBranchId = sourceRegionalBranch.id;
+          })
+          .then(function(){
+            return regionalBranch
+            .findOne({ where: { label: routeFixture.destinationBranchLabel } });
+          })
+          .then(function(destinationBranch){
+            destinationBranchId = destinationBranch.id;
+          })
+          .then(function(){
+            return routeFixture.midNodes;
+          })
+          .map(function(midNodeLabel){
+            return regionalBranch.findOne({  where: { label: midNodeLabel } });
+          })
+          .map(function(midNodeBranch){
+            return midNodeBranch.id;
+          })
+          .then(function(midNodes){
+
+            routeBaseData = {
+              sourceId: sourceBranchId,
+              destinationId: destinationBranchId,
+              midNodes: JSON.stringify(midNodes)
+            };
+            console.log(routeBaseData);
+
+            return route.create(routeBaseData);
+
+          })
+          .then(function(result){
+            if(result){
+              done();
+              return null;
+            }
+            done(result);
+          });
+        });
       })
       .catch(function(err){
-        console.log(err);
         done(err);
       });
     });
