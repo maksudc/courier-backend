6d38a58848ba709ce9ea873c6336125a6cbb4e57
tariqul islam <tariqul.isha@gmail.com>
2016-04-28 00:21:34 +0600

    Fix: VD creation regional branch was not called and set properly

--- routes/order.js
+++ routes/order.js
@@ -128,8 +128,6 @@ router.post('/createByOperator', passport.authenticate('basic', {session: false}
 			}
 		*/
 
-		console.log(req.body);
-
 		orderLogic.createByOperator(req.body, req.user, function(data){
 			res.send(data);
 	});
