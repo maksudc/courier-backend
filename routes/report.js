96ec7ef810efa95d0b868e390fb7fca716d7dabb
tariqul islam <tariqul.isha@gmail.com>
2016-05-04 23:32:45 +0600

    Feature: Money order receive and deliver time set

--- routes/report.js
+++ routes/report.js
@@ -22,8 +22,6 @@ router.get("/" , function(req, res){
 
 router.get("/parcel/cashin", function(req, res){
 
-	console.log(req.query);
-
 	reportLogic.getOrderPaymentData(req.query, req.user, function(err, reportData){
 		if(err) res.send({"status": "error", error: err});
 		else res.send({"status": "success", data: reportData});
