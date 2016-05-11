24d89a9bad5b2d07866a1899ccd4a1f28a939532
tariqul islam <tariqul.isha@gmail.com>
2016-05-12 02:31:48 +0600

    Fix: Reporting revenue

--- logics/reportLogic.js
+++ logics/reportLogic.js
@@ -179,7 +179,7 @@ var findMoneyCashIn = function(params, adminData, next){
 				{payment_time: {$gt: new Date(new Date() - timeRange)}}
 			]
 		},
-		attributes: ['id', 'payable', 'amount', 'sender_mobile', 'receiver_mobile', 'type']
+		attributes: ['id', 'payable', 'amount', 'charge', 'discount', 'sender_mobile', 'receiver_mobile', 'type']
 	}).then(function(moneyOrderData){
 		next(null, moneyOrderData);
 	}).catch(function(err){
@@ -226,7 +226,7 @@ var findMoneyCashOut = function(params, adminData, next){
 				{delivery_time: {$gt: new Date(new Date() - timeRange)}}
 			]
 		},
-		attributes: ['id', 'amount', 'payable', 'sender_mobile', 'receiver_mobile', 'type']
+		attributes: ['id', 'amount', 'charge', 'discount', 'payable', 'sender_mobile', 'receiver_mobile', 'type']
 	}).then(function(moneyOrderData){
 		next(null, moneyOrderData);
 	}).catch(function(err){
