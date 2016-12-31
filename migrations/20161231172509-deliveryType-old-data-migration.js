dd5454e606b7f26413521c3ea0a29ecf521c157b
Md. Maksud Alam Chowdhury <mc65799@gmail.com>
2017-01-01 02:18:45 +0600

    adds recommended change for training site

---
+++ migrations/20161231172509-deliveryType-old-data-migration.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var DB = require("../models/");
+var orderModel = DB.sequelize.models.order;
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    //var thresholdDate = Date.parse("2016-12-24 23:59:59");
+    var thresholdDate = "2016-12-24 23:59:59";
+    var defaultDeliveryType = "branch";
+
+    return orderModel.update(
+      { deliveryType: defaultDeliveryType } ,
+      { where: { confirm_time:{ $lt: thresholdDate } } }
+    ).then(function(result){
+      return true;
+    }).catch(function(err){
+      if(err){
+          console.error(err.stack);
+      }
+    });
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
