ef863697b58134493d15b2ca3b49efa72b921072
Md. Maksud Alam Chowdhury <mc65799@gmail.com>
2017-01-07 00:37:11 +0600

    server time adjustment for converting existing deliveryType to branch. Since from now on deliveryType can be set explicitely

--- migrations/20161231172509-deliveryType-old-data-migration.js
+++ migrations/20161231172509-deliveryType-old-data-migration.js
@@ -13,7 +13,7 @@ module.exports = {
       return queryInterface.createTable('users', { id: Sequelize.INTEGER });
     */
     //var thresholdDate = Date.parse("2016-12-24 23:59:59");
-    var thresholdDate = "2016-12-24 23:59:59";
+    var thresholdDate = "2017-01-06 23:59:59";
     var defaultDeliveryType = "branch";
 
     return orderModel.update(
@@ -36,5 +36,18 @@ module.exports = {
       Example:
       return queryInterface.dropTable('users');
     */
+    var thresholdDate = "2017-01-06 23:59:59";
+    var defaultDeliveryType = "home";
+
+    return orderModel.update(
+      { deliveryType: defaultDeliveryType } ,
+      { where: { confirm_time:{ $lt: thresholdDate } } }
+    ).then(function(result){
+      return true;
+    }).catch(function(err){
+      if(err){
+          console.error(err.stack);
+      }
+    });
   }
 };
