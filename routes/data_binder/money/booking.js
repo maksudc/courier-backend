9d5682bb8bc3d297458268ea7313c04465e21f9c
Md. Maksud Alam Chowdhury <mc65799@gmail.com>
2017-03-29 16:35:40 +0600

    adds pagination for api

--- routes/data_binder/money/booking.js
+++ routes/data_binder/money/booking.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var HttpStatus = require("http-status-codes");
 var moneyLogic = require("./../../../logics/moneyLogic");
 var DB = require("./../../../models/index");
-var moneyModel = DB.sequelize.money;
+var moneyModel = DB.sequelize.models.money;
 
 var DataTableHelper = function(tableConfig){
 	this.config = tableConfig;
@@ -62,26 +62,41 @@ DataTableHelper.prototype.getDraw = function(){
 };
 
 DataTableHelper.prototype.getWhere = function(){
-	whereStatement = "";
+	whereStatement = [];
 	for(I=0; I < this.config.columns.length ; I++){
 		columnDef = this.config.columns[I];
 
 		if(columnDef.searchable && columnDef["search"]["value"]){
 
-			if(whereStatement){
-				whereStatement = whereStatement + " AND ";
-			}
+			currentQuery = {};
 			operationType = "=";
 			if(columnDef["operation"]){
 				operationType = columnDef["operation"];
+				// Operation wise nexted complex where selection
 			}
 			if(columnDef["search"]["regex"]){
 				operationType = "LIKE";
 			}
-			whereStatement = whereStatement + " " + columnDef["data"] + " " + operationType +" '" + columnDef["search"]["value"] + "' ";
+
+			if(["=" , "$eq"].indexOf(operationType) > -1){
+					currentQuery[columnDef["data"]] = columnDef["search"]["value"];
+			}else if(["LIKE"].indexOf(operationType) > -1){
+					currentQuery[columnDef["data"]] = {
+						"$like": columnDef["search"]["value"]
+					};
+			}
+
+			whereStatement.push(currentQuery);
 		}
 	}
-	return whereStatement;
+
+	combinedQuery = {};
+	if(whereStatement.length > 0){
+		combinedQuery = {
+			"$and": whereStatement
+		};
+	}
+	return combinedQuery;
 };
 
 router.get('/', function(req, res){
@@ -90,42 +105,92 @@ router.get('/', function(req, res){
 	console.log(tableHelper.config);
 	console.log(tableHelper.getWhere());
 	console.log(tableHelper.getOrder());
+	console.log(tableHelper.getOffset());
+	console.log(tableHelper.getLimit());
 
-	moneyLogic.findBookings(req.query , function(err, data){
-		if(err){
-			console.error(err.stack);
-			res.status(HttpStatus.INTERNAL_SERVER_ERROR);
-			res.send({"status": "error", error: err});
-			return;
-		}
+	queryParams  = {};
+	queryParams["limit"] = tableHelper.getLimit();
+	queryParams["offset"] = tableHelper.getOffset();
+	queryParams["where"] = tableHelper.getWhere();
 
-		moneyLogic
-		.getTotalMoneyCount(null)
-		.then(function(c){
+	var resultData = {};
+	resultData["draw"] = tableHelper.getDraw();
 
-			recordsTotal = data.objects.count;
-			recordsFiltered = data.objects.rows.length;
+	moneyModel
+		.findAndCountAll(queryParams)
+		.then(function(moneyOrderList){
 
-			queryDrawCount = req.query["draw"];
-			if(!queryDrawCount){
-				queryDrawCount = 1;
-			}
+				resultData["data"] = moneyOrderList;
+				resultData["recordsTotal"] = moneyOrderList.count;
+				resultData["recordsFiltered"] = moneyOrderList.count;
 
-			res.status(HttpStatus.OK);
-			res.send({
-				"status": "success",
-				data: data ,
-				recordsTotal: recordsTotal ,
-				recordsFiltered: recordsFiltered,
-				draw: queryDrawCount
-			});
+				res.status(HttpStatus.OK);
+				res.send(resultData);
+				//resultData["pagination"] = {};
+				// resultData["pagination"]["maxPage"] = Math.ceil(resultData["objects"].count / queryParams["limit"]);
+				// if(resultData["pagination"]["maxPage"]==0){
+				// 	resultData["pagination"]["maxPage"] = 1;
+				// }
+				//
+				// if(params.page >= 1 && params.page < resultData["pagination"]["maxPage"] ){
+				// 	resultData["pagination"]["nextPageNo"] = params.page + 1;
+				// }else{
+				// 	resultData["pagination"]["nextPageNo"] = null;
+				// }
+				//
+				// if(params.page > 1 && params.page <= resultData["pagination"]["maxPage"]){
+				// 	resultData["pagination"]["previousPageNo"] = params.page -1;
+				// }else{
+				// 	resultData["pagination"]["previousPageNo"] = null;
+				// }
+
+				// resultData["pagination"]["page"] = params.page;
+				// resultData["pagination"]["limit"] = params.limit;
+				// next(null, resultData);
 		})
 		.catch(function(err){
+			if(err){
+				console.error(err.stack);
+			}
 			res.status(HttpStatus.INTERNAL_SERVER_ERROR);
-			res.send({"status": "error", error: err});
-		});
-		// else if(!data) res.send({"status": "error", data: []});
+			res.send({ error:"Internal Server error occured" });
 	});
+
+	// moneyLogic.findBookings(req.query , function(err, data){
+	// 	if(err){
+	// 		console.error(err.stack);
+	// 		res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+	// 		res.send({"status": "error", error: err});
+	// 		return;
+	// 	}
+	//
+	// 	moneyLogic
+	// 	.getTotalMoneyCount(null)
+	// 	.then(function(c){
+	//
+	// 		recordsTotal = data.objects.count;
+	// 		recordsFiltered = data.objects.rows.length;
+	//
+	// 		queryDrawCount = req.query["draw"];
+	// 		if(!queryDrawCount){
+	// 			queryDrawCount = 1;
+	// 		}
+	//
+	// 		res.status(HttpStatus.OK);
+	// 		res.send({
+	// 			"status": "success",
+	// 			data: data ,
+	// 			recordsTotal: recordsTotal ,
+	// 			recordsFiltered: recordsFiltered,
+	// 			draw: queryDrawCount
+	// 		});
+	// 	})
+	// 	.catch(function(err){
+	// 		res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+	// 		res.send({"status": "error", error: err});
+	// 	});
+	// 	// else if(!data) res.send({"status": "error", data: []});
+	// });
 
 });
 
