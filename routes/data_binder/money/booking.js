f76d23140f82238f9a4a8c635883d28246f0432a
Md. Maksud Alam Chowdhury <maksud.alam@iappdragon.com>
2017-03-31 13:37:54 +0600

    adds fix for booked status restriction removal

--- routes/data_binder/money/booking.js
+++ routes/data_binder/money/booking.js
@@ -25,7 +25,7 @@ router.get('/', function(req, res){
 	whereQuery = null;
 
 	extraQuery = {
-		"status": "draft"
+		//"status": "draft"
 	};
 	if(userObj){
 		//&& !adminUtils.isPrivileged(userObj.getRole())){
