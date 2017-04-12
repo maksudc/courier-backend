70f74e0fc42e4c793bd1484f29175974fcf9a886
Md. Maksud Alam Chowdhury <maksud.alam@iappdragon.com>
2017-04-12 18:56:41 +0600

    adds migration script needs do, undo checking

--- migrations/20170412010647-payment-branch-datafix-migration.js
+++ migrations/20170412010647-payment-branch-datafix-migration.js
@@ -1,8 +1,11 @@
 'use strict';
 
+var Promise = require("bluebird");
+
 var DB = require("../models/");
 var orderModel = DB.sequelize.models.order;
 var adminModel = DB.sequelize.models.admin;
+var branchUtils = require("../utils/branch");
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
@@ -16,35 +19,95 @@ module.exports = {
     return
     orderModel.findAll({
       where:{
-        "$and":[
-          { payment_status: "paid" },
+        "$or":[
+          { payment_branch_migrate_affected: true },
           {
-            payment_operator:{
-              "$ne": null
-            },
-          },
-          {
-            payment_tag:{
-              '$eq': null
-            }
-          },
-          {
-            payment_hub_type:{
-              "$eq": null
-            }
-          },
-          {
-            payment_hub:{
-              "$eq": null
-            }
+            "$and":[
+              { payment_status: "paid" },
+              {
+                payment_operator:{
+                  "$ne": null
+                },
+              },
+              {
+                payment_tag:{
+                  '$eq': null
+                }
+              },
+              {
+                payment_hub_type:{
+                  "$eq": null
+                }
+              },
+              {
+                payment_hub:{
+                  "$eq": null
+                }
+              }
+            ]
           }
         ]
       }
     })
-    .map(function(){
+    .map(function(orderInstance){
 
+      operator_email = orderInstance.payment_operator;
+      return Promise.all([
+        orderInstance,
+        adminModel.findOne({
+          where: {
+            "email": operator_email
+          }
+        })
+      ]);
+    })
+    .map(function(bundle){
+      orderInstance = bundle[0];
+      adminInstance = bundle[1];
+
+      payment_hub_type = null;
+      payment_hub = null;
+      payment_tag = null;
+
+      if(adminInstance){
+
+        if(adminInstance.sub_branch_id){
+          payment_hub_type = "sub";
+          payment_hub = adminInstance.sub_branch_id;
+        }else if(adminInstance.regional_branch_id){
+          payment_hub_type = "regional";
+          payment_hub = adminInstance.regional_branch_id;
+        }
+
+        if(payment_hub_type == branchUtils.sanitizeBranchType(orderInstance.exit_branch_type) && payment_hub == orderInstance.exit_branch){
+          payment_tag = "delivery";
+        }else if(payment_hub_type == branchUtils.sanitizeBranchType(orderInstance.entry_branch_type) && payment_hub == orderInstance.entry_branch){
+          payment_tag = "booking";
+        }
+
+        if(payment_hub_type){
+          orderInstance.payment_hub_type = payment_hub_type;
+        }
+        if(payment_hub){
+          orderInstance.payment_hub = payment_hub;
+        }
+        if(payment_tag){
+          orderInstance.payment_tag = payment_tag;
+        }
+      }
+
+      orderInstance.payment_branch_migrate_affected = true;
+
+      return orderInstance.save();
+    })
+    .then(function(results){
+      return true;
+    }).catch(function(err){
+      if(err){
+        console.error(err.stack);
+      }
+      return false;
     });
-
   },
 
   down: function (queryInterface, Sequelize) {
@@ -55,5 +118,28 @@ module.exports = {
       Example:
       return queryInterface.dropTable('users');
     */
+    return
+    orderModel.findAll({
+      where:{
+        payment_branch_migrate_affected: true
+      }
+    })
+    .map(function(orderInstance){
+
+      orderInstance.payment_hub = null;
+      orderInstance.payment_hub_type = null;
+      orderInstance.payment_tag = null;
+      orderInstance.payment_branch_migrate_affected = false;
+
+      return orderInstance.save();
+    })
+    .then(function(results){
+      return true;
+    }).catch(function(err){
+      if(err){
+        console.error(err.stack);
+      }
+      return false;
+    });
   }
 };
