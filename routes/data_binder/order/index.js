11a334c18a11f06651d3ca8b0d14239473dd7641
Md. Maksud Alam Chowdhury <mc65799@gmail.com>
2017-05-04 22:48:15 +0600

    adds draft order view

--- routes/data_binder/order/index.js
+++ routes/data_binder/order/index.js
@@ -4,4 +4,7 @@ var router = express.Router();
 var stashViewDataBinder = require("./viewStash");
 router.use('/stash/view', stashViewDataBinder);
 
+var draftViewDataBinder = require("./viewDraft");
+router.use('/draft/view' , draftViewDataBinder);
+
 module.exports = router;
