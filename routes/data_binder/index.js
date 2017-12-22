75aea6325b3fa6a992cf40f6d63e9b4a3283faaa
Md. Maksud Alam Chowdhury <mc65799@gmail.com>
2017-12-23 00:45:22 +0600

    adds panic mode middleware to only applicable data listing routes

--- routes/data_binder/index.js
+++ routes/data_binder/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+// Panic mode detection and header addition
+var panicMiddleware = require("./../../middleware/panic");
+router.use(panicMiddleware);
+
 var moneyDataBinderRouter = require("./money");
 router.use('/money', moneyDataBinderRouter);
 
