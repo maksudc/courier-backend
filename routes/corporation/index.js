eb07dfb1b5c654ac4d78f93d24f821ae46ecbf77
Md. Maksud Alam Chowdhury <mc65799@gmail.com>
2018-02-14 22:25:31 +0600

    phone addition removal from basic update

--- routes/corporation/index.js
+++ routes/corporation/index.js
@@ -51,8 +51,6 @@ router.get("/:corporationId" , function(req, res){
 
 router.put("/:corporationId", upload.array(), function(req, res){
 
-  console.log(req.body);
-
   corporationParams = req.body.corporation;
 
   finalClientNumbers = req.body.clients;
@@ -68,45 +66,46 @@ router.put("/:corporationId", upload.array(), function(req, res){
     };
 
     return corporationModel
-    .update(corporationParams, corporationQuery)
-    .then(function(result){
+    .update(corporationParams, corporationQuery);
+    // .then(function(result){
+    //
+    //   return clientModel.findAll({
+    //     where: {
+    //       corporationId: req.params.corporationId
+    //     },
+    //     attributes: [ "mobile", "full_name" ],
+    //     transaction: t
+    //   });
+    // })
+    // .then(function(corporateEmployees){
+    //
+    //   removableEmployees = [];
+    //   for(I=0; I < corporateEmployees.length; I++){
+    //     employee = corporateEmployees[I];
+    //     if(!finalClientSet.has(employee.dataValues["mobile"])){
+    //       removableEmployees.push(employee.dataValues["mobile"]);
+    //     }
+    //   }
+    //
+    //   existingClientSet = new Set();
+    //   for(I=0; I < corporateEmployees.length; I++){
+    //     employee = corporateEmployees[I];
+    //     existingClientSet.add(employee.dataValues["mobile"]);
+    //   }
+    //
+    //   addableEmployees = [];
+    //   for(I =0 ; I < finalClientNumbers.length; I++){
+    //     if(!existingClientSet.has(finalClientNumbers[I])){
+    //       addableEmployees.push(finalClientNumbers[I]);
+    //     }
+    //   }
+    //
+    //   return  Promise.all([
+    //     addableEmployees.length > 0 ? clientModel.update({corporationId: req.params.corporationId} , { where: { mobile:{ "$in": addableEmployees } }, transaction: t  }): [],
+    //     removableEmployees.length > 0 ? clientModel.update({corporationId: null } , { where: { mobile:{ "$in": removableEmployees } }, transaction: t  }): []
+    //   ]);
+    // });
 
-      return clientModel.findAll({
-        where: {
-          corporationId: req.params.corporationId
-        },
-        attributes: [ "mobile", "full_name" ],
-        transaction: t
-      });
-    })
-    .then(function(corporateEmployees){
-
-      // removableEmployees = [];
-      // for(I=0; I < corporateEmployees.length; I++){
-      //   employee = corporateEmployees[I];
-      //   if(!finalClientSet.has(employee.dataValues["mobile"])){
-      //     removableEmployees.push(employee.dataValues["mobile"]);
-      //   }
-      // }
-      //
-      // existingClientSet = new Set();
-      // for(I=0; I < corporateEmployees.length; I++){
-      //   employee = corporateEmployees[I];
-      //   existingClientSet.add(employee.dataValues["mobile"]);
-      // }
-      //
-      // addableEmployees = [];
-      // for(I =0 ; I < finalClientNumbers.length; I++){
-      //   if(!existingClientSet.has(finalClientNumbers[I])){
-      //     addableEmployees.push(finalClientNumbers[I]);
-      //   }
-      // }
-      //
-      // return  Promise.all([
-      //   addableEmployees.length > 0 ? clientModel.update({corporationId: req.params.corporationId} , { where: { mobile:{ "$in": addableEmployees } }, transaction: t  }): [],
-      //   removableEmployees.length > 0 ? clientModel.update({corporationId: null } , { where: { mobile:{ "$in": removableEmployees } }, transaction: t  }): []
-      // ]);
-    });
   })
   .then(function(result){
 
