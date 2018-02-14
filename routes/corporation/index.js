98e6ae3f2870469a72764e683c84e64f261b8e1e
Md. Maksud Alam Chowdhury <maksud.alam@iappdragon.com>
2018-02-14 17:34:27 +0600

    corporation edit basic info api

--- routes/corporation/index.js
+++ routes/corporation/index.js
@@ -5,6 +5,7 @@ var authMiddleware  = require("./../../middleware/auth");
 var DB = require("./../../models/index");
 var sequelize = DB.sequelize;
 var corporationModel = sequelize.models.corporation;
+var clientModel = sequelize.models.client;
 var HttpStatusCodes = require("http-status-codes");
 var multer = require("multer");
 var upload = multer();
@@ -50,8 +51,77 @@ router.get("/:corporationId" , function(req, res){
 
 router.put("/:corporationId", upload.array(), function(req, res){
 
-  params = req.body;
-  
+  corporationParams = req.body.corporation;
+
+  finalClientNumbers = req.body.clients;
+  finalClientSet= new Set(finalClientNumbers);
+
+  sequelize.transaction(function(t){
+
+    corporationQuery = {
+      where: {
+        id: req.params.corporationId
+      },
+      transaction: t
+    };
+
+    return corporationModel
+    .update(corporationParams, corporationQuery)
+    .then(function(result){
+
+      return clientModel.findAll({
+        where: {
+          corporationId: req.params.corporationId
+        },
+        attributes: [ "mobile", "full_name" ],
+        transaction: t
+      });
+    })
+    .then(function(corporateEmployees){
+
+      // removableEmployees = [];
+      // for(I=0; I < corporateEmployees.length; I++){
+      //   employee = corporateEmployees[I];
+      //   if(!finalClientSet.has(employee.dataValues["mobile"])){
+      //     removableEmployees.push(employee.dataValues["mobile"]);
+      //   }
+      // }
+      //
+      // existingClientSet = new Set();
+      // for(I=0; I < corporateEmployees.length; I++){
+      //   employee = corporateEmployees[I];
+      //   existingClientSet.add(employee.dataValues["mobile"]);
+      // }
+      //
+      // addableEmployees = [];
+      // for(I =0 ; I < finalClientNumbers.length; I++){
+      //   if(!existingClientSet.has(finalClientNumbers[I])){
+      //     addableEmployees.push(finalClientNumbers[I]);
+      //   }
+      // }
+      //
+      // return  Promise.all([
+      //   addableEmployees.length > 0 ? clientModel.update({corporationId: req.params.corporationId} , { where: { mobile:{ "$in": addableEmployees } }, transaction: t  }): [],
+      //   removableEmployees.length > 0 ? clientModel.update({corporationId: null } , { where: { mobile:{ "$in": removableEmployees } }, transaction: t  }): []
+      // ]);
+    });
+  })
+  .then(function(result){
+
+    res.status(HttpStatusCodes.OK);
+    res.send({
+      message: "Updated successfully",
+      id: req.params.corporationId,
+      type: "corporation"
+    });
+  })
+  .catch(function(err){
+    res.error(err);
+
+    res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR);
+    res.send(err);
+  });
+
 });
 
 router.delete("/:corporationId", upload.array(), function(req, res){
