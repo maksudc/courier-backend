52b44a02768d3f8b1c6dcd8911b297a72872a8a1
Md. Maksud Alam Chowdhury <maksud.alam@iappdragon.com>
2018-03-23 18:39:27 +0600

    disabling the status propagation for tracking

--- hooks/order.js
+++ hooks/order.js
@@ -42,8 +42,6 @@ function sanitizeBranchType(branchType){
 
 order.hook("beforeCreate" , function(instance , options , next){
 
-  console.log("Order before creation");
-
   if(!instance.current_hub){
     instance.dataValues.current_hub = instance.entry_branch;
     instance.current_hub = instance.entry_branch;
@@ -59,210 +57,47 @@ order.hook("beforeCreate" , function(instance , options , next){
   return next();
 });
 
+order.hook("afterCreate" , function(order , options){
+  /**
+    * Create a tracker item corresponding to the order
+  **/
+  order
+  .getTracker()
+  .then(function(currentTrackerItem){
+    if(!currentTrackerItem){
 
-order.hook("afterUpdate" , function(instance , options , next){
+      var trackerData = {};
 
-  // if the order is in running state
-  // all the items inside it should be also in running state as well
+      if(order.entry_branch_type == "regional-branch"){
+        trackerData.sourceBranchType = "regional";
+      }else{
+        trackerData.sourceBranchType = "sub";
+      }
+      trackerData.sourceBranchId = parseInt(order.entry_branch);
 
-  var snapshotInstance = instance._previousDataValues;
-  var updatedInstance = instance.dataValues;
+      if(order.exit_branch_type == "regional-branch"){
+        trackerData.destinationBranchType = "regional";
+      }else{
+        trackerData.destinationBranchType = "sub";
+      }
+      trackerData.destinationBranchId = parseInt(order.exit_branch);
 
-  var orderInstance = instance;
-  //console.log("instances : " + JSON.stringify(orderInstances));
-  console.log(" on order after update hook for: "+ orderInstance.uuid);
-    //console.log(orderInstance);
-    //console.log(options);
-  console.log(" Whether shipment changed ? " + orderInstance.changed('shipmentUuid'));
+      trackerData.currentBranchType = trackerData.sourceBranchType;
+      trackerData.currentBranchId = trackerData.sourceBranchId;
 
-  var pstatus = Promise.resolve(null);
-  var pshipment = Promise.resolve(null);
+      trackerData.previousBranchType = trackerData.sourceBranchType;
+      trackerData.previousBranchId = trackerData.sourceBranchId;
 
-  if(instance.changed('status')){
+      trackerData.trackableType = "order";
+      trackerData.trackableId = order.uuid;
 
-    if(updatedInstance.status == 'running' || updatedInstance.status == "received" || updatedInstance.status == "reached" || updatedInstance.status == "stocked"){
-
-      pstatus =instance
-      .getItems()
-      .map(function(itemInstance){
-          itemInstance.status = updatedInstance.status;
-          return itemInstance.save();
-      })
-      .then(function(results){
-        return Promise.resolve(results);
-      })
-      .then(function(){
-
-        if(updatedInstance.status == 'stocked'){
-
-          console.log("Sending message...");
-
-          branchUtils
-          .getBranchInstance(updatedInstance.exit_branch_type , updatedInstance.exit_branch , null)
-          .then(function(exitBranchInstance){
-
-            if(exitBranchInstance.branchType == "sub"){
-              return Promise.all([ Promise.resolve(exitBranchInstance) , exitBranchInstance.getRegionalBranch() ]);
-            }else{
-              return Promise.all([ Promise.resolve(null) , Promise.resolve(exitBranchInstance) ]);
-            }
-          })
-          .then(function(results){
-
-            messageBranchInstance = {};
-            messageBranchInstance = results[0];
-            if(!messageBranchInstance){
-              messageBranchInstance = results[1];
-            }else{
-              messageBranchInstance.regionalBranch = results[1];
-            }
-            // Send message to the receiver about stocking of his/her order
-
-            // If the order corresponds to VD , the verification code should be for the corresponding money order instead of the
-            // order object
-            if(updatedInstance.type == "value_delivery"){
-
-              instance
-              .getMoney_order()
-              .then(function(moneyOrderItem){
-
-                if(moneyOrderItem){
-
-                  updatedInstance.sender_verification_code = moneyOrderItem.sender_verification_code;
-
-                  content = fs.readFileSync("./views/message/stocked.handlebars");
-                  contentTemplate = handlebars.compile(content.toString());
-                  messsageBody = contentTemplate({ parcelInstance: updatedInstance , branchInstance: messageBranchInstance });
-
-                  messageUtils.sendMessage(updatedInstance.receiver , messsageBody , function(data){
-                    console.log(data);
-                  });
-                }
-              });
-            }else{
-
-              content = fs.readFileSync("./views/message/stocked.handlebars");
-              contentTemplate = handlebars.compile(content.toString());
-              messsageBody = contentTemplate({ parcelInstance: updatedInstance , branchInstance: messageBranchInstance });
-
-              messageUtils.sendMessage(updatedInstance.receiver , messsageBody , function(data){
-                console.log(data);
-              });
-            }
-          });
-        }
-      });
-
-    }else if(updatedInstance.status == "delivered"){
-
-      // Insert into tracker logs for final delivery of the order
-      pstatus = instance
-      .getTracker()
-      .then(function(trackerInstance){
-
-        var trackerLogData = {};
-
-        trackerLogData.action = "delivered";
-        trackerLogData.trackerId = trackerInstance.uuid;
-        if(trackerInstance.currentBranchType){
-            trackerLogData.branchType = trackerInstance.currentBranchType;
-        }
-        trackerLogData.branchId = trackerInstance.currentBranchId;
-
-        var eventDateTime = moment.utc();
-        trackerLogData.eventDateTime = eventDateTime;
-        trackerLogData.createdAt = eventDateTime;
-        trackerLogData.updatedAt = eventDateTime;
-
-        return trackerLog
-        .create(trackerLogData);
-      })
-      .then(function(trackerLogItem){
-        //return next();
-      })
-      .then(function(){
-
-        //Noe mark the items under this order as delivered
-        return item.update({ status: 'delivered' } , {  where:{ orderUuid: updatedInstance.uuid } , individualHooks:true });
-      })
-      .then(function(results){
-
-          if(updatedInstance.type == "general"){
-            // General delivery
-            // Send message to the sender about delivery of his/her order to the receiver
-            content = fs.readFileSync("./views/message/delivered.handlebars");
-            contentTemplate = handlebars.compile(content.toString());
-            messageBody = contentTemplate({ parcelInstance: updatedInstance });
-
-            messageUtils.sendMessage(updatedInstance.sender , messageBody , function(data){
-              console.log(data);
-            });
-
-          }else{
-
-            // branchUtils
-            // .getInclusiveBranchInstance(updatedInstance.entry_branch_type, updatedInstance.entry_branch , next)
-            // .then(function(entryBranchItem){
-            //
-            //   // VD delivery
-            //   // Since the VD parcel is delivered we need to notify the sender that money order is on its way
-            //   // Send message to the sender about delivery of his/her order to the receiver
-            //   content = fs.readFileSync("./views/message/delivered.handlebars");
-            //   contentTemplate = handlebars.compile(content.toString());
-            //   messageBody = contentTemplate({ parcelInstance: updatedInstance });
-            //
-            //   messageUtils.sendMessage(updatedInstance.sender , messageBody , function(data){
-            //     console.log(data);
-            //   });
-            // });
-
-          }
+      sequelize.models.genericTracker
+      .create(trackerData)
+      .then(function(trackerItem){
+         console.log("Tracker Attached to the order ( "+ order.bar_code +" ) with uuid: "+ trackerItem.uuid);
       });
     }
-  }
-
-    if(orderInstance.changed("shipmentUuid")){
-      // changed the shipment , so trigger the change in tracker parent
-
-      pshipment = orderInstance
-      .getShipment()
-      .then(function(shipmentInstance){
-
-        var p1 = shipmentInstance.getTracker();
-        var p2 = orderInstance.getTracker();
-
-        return sequelize.Promise.all([p1 , p2]);
-      })
-      .then(function(results){
-
-        //console.log(" Trackers:  "+ JSON.stringify(results));
-        var parentTrackerInstance = results[0];
-        var childTrackerInstance = results[1];
-
-        if(parentTrackerInstance){
-          if(childTrackerInstance){
-
-            childTrackerInstance.parentTrackerId = parentTrackerInstance.uuid;
-            return childTrackerInstance.save();
-          }
-        }
-
-        return Promise.resolve(0);
-      })
-      .then(function(updateResult){
-        //console.log("Updated : " + updateResult);
-        //return next();
-      });
-
-    }
-
-    return pstatus
-    .then(function(result){
-      return pshipment;
-    })
-    .then(function(result){
-      return next();
-    });
+  });
 });
 
 order.hook("beforeUpdate" , function(instance , options , next){
@@ -270,10 +105,9 @@ order.hook("beforeUpdate" , function(instance , options , next){
   var updatedInstance = instance.dataValues;
   var snapshotInstance = instance._previousDataValues;
 
-  console.log("Snapshot : " + snapshotInstance.uuid);
-  console.log("Instance Id: " + updatedInstance.uuid);
+  console.log("Order Updated : " + snapshotInstance.bar_code);
 
-  console.log(" Snapshot Status:  " + snapshotInstance.status);
+  console.log("Snapshot Status:  " + snapshotInstance.status);
   console.log("Instance status: " + updatedInstance.status);
 
   if(!instance.changed('status')){
@@ -287,9 +121,6 @@ order.hook("beforeUpdate" , function(instance , options , next){
       // set the previous branch to the current branch
       // Get the route for the source and destination
       // set the next branch to the next regional route
-
-      //updatedInstance.previousBranchType = snapshotInstance.currentBranchType;
-      //updatedInstance.previousBranchId = snapshotInstance.currentBranchId;
 
       updatedInstance.current_hub = updatedInstance.entry_branch;
       updatedInstance.current_hub_type = sanitizeBranchType(updatedInstance.entry_branch_type);
@@ -305,39 +136,6 @@ order.hook("beforeUpdate" , function(instance , options , next){
           firstRoute = routes[0];
         }else{
           firstRoute = routes[1];
-
-          // started off directly the regional branch , so send the message since it is running
-          // console.log("starting the message sending to let know the sender of the starting of the journey...");
-          //
-          // branchUtils
-          // .getBranchInstance( updatedInstance.exit_branch_type , updatedInstance.exit_branch , null)
-          // .then(function(exitBranchInstance){
-          //
-          //   if(exitBranchInstance.branchType == "sub"){
-          //     return Promise.all([ Promise.resolve(exitBranchInstance) , exitBranchInstance.getRegionalBranch() ]);
-          //   }else{
-          //     return Promise.all([ Promise.resolve(null) , Promise.resolve(exitBranchInstance) ]);
-          //   }
-          // })
-          // .then(function(results){
-          //
-          //   messageBranchInstance = {};
-          //   messageBranchInstance = results[0];
-          //   if(!messageBranchInstance){
-          //     messageBranchInstance = results[1];
-          //   }else{
-          //     messageBranchInstance.regionalBranch = results[1];
-          //   }
-          //   // Send message to the sender about starting of the journey of his/her order to the receiver
-          //   content = fs.readFileSync("./views/message/start.handlebars");
-          //   contentTemplate = handlebars.compile(content.toString());
-          //   messageBody =  contentTemplate({ parcelInstance: updatedInstance });
-          //
-          //   messageUtils.sendMessage(updatedInstance.sender , messageBody , function(data){
-          //     console.log(data);
-          //   });
-          // });
-
         }
 
         console.log("Adjusted Route is : ");
@@ -698,3 +496,211 @@ order.hook("beforeUpdate" , function(instance , options , next){
 
     return next();
 });
+
+order.hook("afterUpdate" , function(instance , options , next){
+
+  var snapshotInstance = instance._previousDataValues;
+  var updatedInstance = instance.dataValues;
+
+  var orderInstance = instance;
+
+  console.log(" on order after update hook for: "+ orderInstance.bar_code);
+
+  var pstatus = Promise.resolve(null);
+  var pshipment = Promise.resolve(null);
+
+  if(instance.changed('status')){
+
+    if(updatedInstance.status == 'running' || updatedInstance.status == "received" || updatedInstance.status == "reached" || updatedInstance.status == "stocked"){
+
+      pstatus =instance
+      .getItems()
+      .map(function(itemInstance){
+          itemInstance.status = updatedInstance.status;
+          return itemInstance.save();
+      })
+      .then(function(results){
+        return Promise.resolve(results);
+      })
+      .then(function(){
+
+        if(updatedInstance.status == 'stocked'){
+
+          console.log("Sending message...");
+
+          branchUtils
+          .getBranchInstance(updatedInstance.exit_branch_type , updatedInstance.exit_branch , null)
+          .then(function(exitBranchInstance){
+
+            if(exitBranchInstance.branchType == "sub"){
+              return Promise.all([ Promise.resolve(exitBranchInstance) , exitBranchInstance.getRegionalBranch() ]);
+            }else{
+              return Promise.all([ Promise.resolve(null) , Promise.resolve(exitBranchInstance) ]);
+            }
+          })
+          .then(function(results){
+
+            messageBranchInstance = {};
+            messageBranchInstance = results[0];
+            if(!messageBranchInstance){
+              messageBranchInstance = results[1];
+            }else{
+              messageBranchInstance.regionalBranch = results[1];
+            }
+            // Send message to the receiver about stocking of his/her order
+
+            // If the order corresponds to VD , the verification code should be for the corresponding money order instead of the
+            // order object
+            if(updatedInstance.type == "value_delivery"){
+
+              instance
+              .getMoney_order()
+              .then(function(moneyOrderItem){
+
+                if(moneyOrderItem){
+
+                  updatedInstance.sender_verification_code = moneyOrderItem.sender_verification_code;
+
+                  content = fs.readFileSync("./views/message/stocked.handlebars");
+                  contentTemplate = handlebars.compile(content.toString());
+                  messsageBody = contentTemplate({ parcelInstance: updatedInstance , branchInstance: messageBranchInstance });
+
+                  messageUtils.sendMessage(updatedInstance.receiver , messsageBody , function(data){
+                    console.log(data);
+                  });
+                }
+              });
+            }else{
+
+              content = fs.readFileSync("./views/message/stocked.handlebars");
+              contentTemplate = handlebars.compile(content.toString());
+              messsageBody = contentTemplate({ parcelInstance: updatedInstance , branchInstance: messageBranchInstance });
+
+              messageUtils.sendMessage(updatedInstance.receiver , messsageBody , function(data){
+                console.log(data);
+              });
+            }
+          });
+        }
+      });
+
+    }else if(updatedInstance.status == "delivered"){
+
+      // Insert into tracker logs for final delivery of the order
+      pstatus = instance
+      .getTracker()
+      .then(function(trackerInstance){
+
+        var trackerLogData = {};
+
+        trackerLogData.action = "delivered";
+        trackerLogData.trackerId = trackerInstance.uuid;
+        if(trackerInstance.currentBranchType){
+            trackerLogData.branchType = trackerInstance.currentBranchType;
+        }
+        trackerLogData.branchId = trackerInstance.currentBranchId;
+
+        var eventDateTime = moment.utc();
+        trackerLogData.eventDateTime = eventDateTime;
+        trackerLogData.createdAt = eventDateTime;
+        trackerLogData.updatedAt = eventDateTime;
+
+        return trackerLog
+        .create(trackerLogData);
+      })
+      .then(function(trackerLogItem){
+        //return next();
+      })
+      .then(function(){
+
+        //Noe mark the items under this order as delivered
+        return item.update({ status: 'delivered' } , {  where:{ orderUuid: updatedInstance.uuid } , individualHooks:true });
+      })
+      .then(function(results){
+
+          if(updatedInstance.type == "general"){
+            // General delivery
+            // Send message to the sender about delivery of his/her order to the receiver
+            content = fs.readFileSync("./views/message/delivered.handlebars");
+            contentTemplate = handlebars.compile(content.toString());
+            messageBody = contentTemplate({ parcelInstance: updatedInstance });
+
+            messageUtils.sendMessage(updatedInstance.sender , messageBody , function(data){
+              console.log(data);
+            });
+
+          }else{
+
+          }
+      });
+    }
+  }
+
+    if(orderInstance.changed("shipmentUuid")){
+      // changed the shipment , so trigger the change in tracker parent
+
+      pshipment = orderInstance
+      .getShipment()
+      .then(function(shipmentInstance){
+
+        var p1 = shipmentInstance.getTracker();
+        var p2 = orderInstance.getTracker();
+
+        return sequelize.Promise.all([p1 , p2]);
+      })
+      .then(function(results){
+
+        //console.log(" Trackers:  "+ JSON.stringify(results));
+        var parentTrackerInstance = results[0];
+        var childTrackerInstance = results[1];
+
+        if(parentTrackerInstance){
+          if(childTrackerInstance){
+
+            childTrackerInstance.parentTrackerId = parentTrackerInstance.uuid;
+            return childTrackerInstance.save();
+          }
+        }
+
+        return Promise.resolve(0);
+      })
+      .then(function(updateResult){
+        //console.log("Updated : " + updateResult);
+        //return next();
+      });
+
+    }
+
+    return pstatus
+    .then(function(result){
+      return pshipment;
+    })
+    .then(function(result){
+      return next();
+    });
+});
+
+order.hook("beforeDestroy" , function(orderItem , options){
+
+  orderItem
+  .getTracker()
+  .then(function(trackerItem){
+    if(trackerItem){
+        return trackerItem.destroy();
+    }else{
+      return ;
+    }
+  })
+  .then(function(result){
+    //console.log(result);
+  })
+  .catch(function(err){
+    //console.log(err);
+    if(err){
+      console.error(err.stack);
+    }
+  });
+});
+
+return order;
+};
