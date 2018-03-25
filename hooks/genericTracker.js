9286a8f4f6463ffebaeee898c7b7af6f57784e48
Md. Maksud Alam Chowdhury <mc65799@gmail.com>
2018-03-25 19:23:34 +0600

    adds genericTracker destroy hook

--- hooks/genericTracker.js
+++ hooks/genericTracker.js
@@ -27,59 +27,13 @@ genericTracker.hook("afterCreate" , function(trackerInstance , options){
     transaction: options.transaction
   });
 });
-/*
-genericTracker.hook("beforeUpdate" , function(trackerInstance , options , next){
 
-  if(trackerInstance.trackableType == "orderItem" && !moduleSettings.ENABLE_ITEM_TRACKING){
-    return next();
-  }
+genericTracker.hook("beforeDestroy", function(trackerInstance, options){
 
-  var snapshotInstance = trackerInstance._previousDataValues;
-  var updatedInstance = trackerInstance.dataValues;
-
-  var trackerLogData = {};
-  trackerLogData.trackerId = trackerInstance.uuid;
-
-  trackerLogData.branchType = updatedInstance.currentBranchType;
-  trackerLogData.branchId = updatedInstance.currentBranchId;
-
-  var eventDateTime = moment.utc();
-  trackerLogData.eventDateTime = eventDateTime;
-  trackerLogData.createdAt = eventDateTime;
-  trackerLogData.updatedAt = eventDateTime;
-
-  if(snapshotInstance.currentBranchType != updatedInstance.currentBranchType || snapshotInstance.currentBranchId !=updatedInstance.currentBranchId){
-    // current branch is changed , so most possible the tracked item has received in a branch
-    // So the action will be `entrance`
-    trackerLogData.action = "entrance";
-
-    if(updatedInstance.currentBranchType == updatedInstance.destinationBranchType){
-      if(updatedInstance.currentBranchId == updatedInstance.destinationBranchId){
-        // the item is reached
-        // So mark the tracker log as reached
-        trackerLogData.action = "reached";
-      }
-    }
-  }
-  else if(snapshotInstance.nextBranchType != updatedInstance.nextBranchType || snapshotInstance.nextBranchId !=updatedInstance.nextBranchId){
-    // next branch is changed
-    // Most possibly the tracked item is marked as running
-    // So the action will be `exit`
-    trackerLogData.action = "exit";
-  }
-
-  if(trackerLogData.action){
-
-    trackerLogData.createdAt = new Date();
-
-    return trackerLog
-    .create(trackerLogData)
-    .then(function(trackerLogInstance){
-      return next();
+    return trackerLog.destroy({
+      where: {
+        trackerId: trackerInstance.uuid
+      },
+      transaction: options.transaction
     });
-  }else{
-
-    return next();
-  }
 });
-*/
