50c94fc3aa00b6adeb3e0857b42833b37a9cc07f
Md. Maksud Alam Chowdhury <mc65799@gmail.com>
2018-07-17 18:37:03 +0600

    adds NULL able client referrer validation

--- utils/common.js
+++ utils/common.js
@@ -5,5 +5,14 @@ function getErrorMessage(err){
   }
   return message;
 }
-
 exports.getErrorMessage = getErrorMessage;
+
+module.exports.isNull = function($val){
+  if(!$val){
+    return true;
+  }else if($val == "null"){
+    return true;
+  }else{
+    return false;
+  }
+};
