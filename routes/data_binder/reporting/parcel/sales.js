99120b6bfc116225aa5a8f699f228c5306e44763
sifan <sifan@iappdragon.com>
2018-09-18 19:20:15 +0600

    operator constant removed

--- routes/data_binder/reporting/parcel/sales.js
+++ routes/data_binder/reporting/parcel/sales.js
@@ -10,7 +10,6 @@ var DataTableHelper = require("./../../../../utils/data_binder/dataTable");
 var passport = require("passport");
 var moment = require("moment-timezone");
 var timezoneConfig = require("./../../../../config/timezone");
-const Op = DB.sequelize.Op;
 router.use(passport.authenticate("basic" , {session: false}));
 
 var panicMiddleware = require("./../../../../middleware/panic");
