5b608eb72447e025a13b6319fe9731aa13b71441
Md. Maksud Alam Chowdhury <mc65799@gmail.com>
2018-09-29 00:18:54 +0600

    initiates directory

---
+++ scripts/orderRepair.js
@@ -0,0 +1,13 @@
+var DB = require("../models");
+var sequelize = DB.sequelize;
+var Sequelize = DB.Sequelize;
+
+var orderModel = DB.sequelize.models.order;
+var adminModel = DB.sequelize.models.admin;
+
+var Promise = require("bluebird");
+
+var findErrorOrderBarcodes = function(){
+
+  //orderModel.
+};
