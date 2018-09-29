121d03735816000cdd7f33984f8af58f32341c95
Md. Maksud Alam Chowdhury <mc65799@gmail.com>
2018-09-29 13:54:19 +0600

    working on promisified request for api fetching and validation for orders

--- scripts/orderRepair.js
+++ scripts/orderRepair.js
@@ -6,8 +6,54 @@ var orderModel = DB.sequelize.models.order;
 var adminModel = DB.sequelize.models.admin;
 
 var Promise = require("bluebird");
+var request = require("request-promise");
+var btoa = require("btoa");
 
 var findErrorOrderBarcodes = function(){
 
-  //orderModel.
+  var totalOrderCount = 0;
+
+  var privilegedAdmin = null;
+
+  adminModel.findAll({
+    where: {
+      role: "super_admin",
+    },
+    limit: 1
+  })
+  .then(function(admin){
+    privilegedAdmin = admin;
+  })
+  .then(function(){
+
+    return
+    orderModel.findAndCountAll({
+      attributes: ["uuid", "bar_code", "status", "type"],
+      where:{
+        status:{
+          "$notIn": ["stocked", "delivered"]
+        },
+        order: "bar_code ASC"
+      }
+    });
+  })
+  .then(function(result){
+
+    totalOrderCount = result.count;
+
+    return Promise.resolve(result.rows);
+  })
+  .map(function(orderInstance){
+
+    options = {
+      method: "GET",
+      uri: "/order/getOrderView/" + orderInstance.get("uuid"),
+      headers:{
+        'Authorization': 'Basic ' + btoa(privilegedAdmin.username + ":" + privilegedAdmin.password)
+      },
+      json: true
+    };
+
+    return request(options);
+  });
 };
