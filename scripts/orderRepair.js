4d0d88b4b9cd8cedb5b3ea75dc8d6f88c8c07dd3
Md. Maksud Alam Chowdhury <mc65799@gmail.com>
2018-09-30 00:26:44 +0600

    adding script for reparing job of corrupted orders

--- scripts/orderRepair.js
+++ scripts/orderRepair.js
@@ -9,7 +9,9 @@ var Promise = require("bluebird");
 var request = require("request-promise");
 var btoa = require("btoa");
 
-var findErrorOrderBarcodes = function(){
+var fs = require("fs");
+
+var findErrorOrderBarcodes = function(limit, offset){
 
   var totalOrderCount = 0;
 
@@ -26,15 +28,16 @@ var findErrorOrderBarcodes = function(){
   })
   .then(function(){
 
-    return
-    orderModel.findAndCountAll({
+      return orderModel.findAndCountAll({
       attributes: ["uuid", "bar_code", "status", "type"],
       where:{
         status:{
           "$notIn": ["stocked", "delivered"]
-        },
-        order: "bar_code ASC"
-      }
+        }
+      },
+      order: "bar_code ASC",
+      limit: limit,
+      offset: offset
     });
   })
   .then(function(result){
@@ -45,15 +48,123 @@ var findErrorOrderBarcodes = function(){
   })
   .map(function(orderInstance){
 
+    var orderUuid = orderInstance.get("uuid");
+
     options = {
       method: "GET",
-      uri: "/order/getOrderView/" + orderInstance.get("uuid"),
+      uri: "/order/getOrderView/" + orderUuid,
       headers:{
         'Authorization': 'Basic ' + btoa(privilegedAdmin.username + ":" + privilegedAdmin.password)
       },
       json: true
     };
 
-    return request(options);
+    return request(options)
+    .then(function(response){
+      var isValid = response["status"] == "success" && response["data"] && response["data"]["orderData"];
+      return Promise.resolve([orderInstance, isValid]);
+    })
+    .catch(function(err){
+      console.error(err);
+      return Promise.resolve([orderInstance, null]);
+    });
+  })
+  .map(function(complexResult){
+
+    orderInstance = complexResult[0];
+    isValid = complexResult[1];
+
+    if(isValid === false){
+      return Promise.resolve(orderInstance.get('bar_code'));
+    }
+    return null;
+  })
+  .then(function(results){
+
+    invalidBarCodes = [];
+    for(I=0; I < results.length; I++){
+      if(results[I]){
+        invalidBarCodes.push(results[I]);
+      }
+    }
+
+    return Promie.resolve(invalidBarCodes);
   });
 };
+
+module.exports.findErrorOrderBarcodes = findErrorOrderBarcodes;
+
+var deleteBarcodes = function(orderBarcodes){
+
+  orderModel.delete({
+    where:{
+      bar_code: {
+        "$in": orderBarcodes
+      }
+    }
+  })
+  .then(function(result){
+    console.log(result);
+  })
+  .catch(function(err){
+    console.error(err);
+  });
+};
+
+module.exports.deleteBarcodes = deleteBarcodes;
+
+var findAndSaveInvalidOrders = function(){
+
+  var BASE_LIMIT = 100;
+  var OUTPUT_FILE_PATH  = "./scripts/invalid_bar_codes.txt";
+
+  orderModel.count({
+    where:{
+      status:{
+        "$notIn": ["stocked", "delivered"]
+      }
+    }
+  })
+  .then(function(totalCount){
+
+    searchTracker = [];
+    pageCount = totalCount / BASE_LIMIT;
+
+    for(I=0; I <= pageCount; I++){
+      searchTracker.push({
+        "limit": BASE_LIMIT,
+        "offset": I * BASE_LIMIT
+      });
+    }
+
+    return Promise.resolve(searchTracker);
+  })
+  .map(function(complexResult){
+
+    limit = complexResult[0];
+    offset = complexResult[1];
+
+    return findErrorOrderBarcodes(limit, offset);
+  })
+  .map(function(invalidBarCodes){
+
+    invalidBarCodeString = invalidBarCodes.join(",");
+
+    fs.appendFile(OUTPUT_FILE_PATH, invalidBarCodeString, function(err) {
+      if(err) {
+          return console.error(err);
+      }
+      console.log(invalidBarCodeString + " was appended");
+    });
+
+    return true;
+  })
+  .then(function(results){
+    
+  })
+  .catch(function(err){
+    console.error(err);
+  });
+};
+
+module.exports.findAndSaveInvalidOrders = findAndSaveInvalidOrders;
