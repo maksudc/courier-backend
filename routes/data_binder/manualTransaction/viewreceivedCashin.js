676e9291e5a4b229d6f92d306e8bb3f0a90d7b28
sifan <sifansetu@gmail.com>
2019-01-01 00:04:32 +0600

    code refactored

--- routes/data_binder/manualTransaction/viewreceivedCashin.js
+++ routes/data_binder/manualTransaction/viewreceivedCashin.js
@@ -1,8 +1,6 @@
 var express = require("express");
 var router = express.Router();
-var multer = require("multer");
 var HttpStatus = require("http-status-codes");
-var branchUtils = require("./../../../utils/branch");
 var DB = require("./../../../models/index");
 var cashinModel = DB.sequelize.models.manualTransactions;
 var DataTableHelper = require("./../../../utils/data_binder/dataTable");
@@ -12,10 +10,6 @@ var panicUtils = require("./../../../utils/panic");
 router.get('/', function (req, res) {
 
     tableHelper = new DataTableHelper(req.query);
-    // console.log(JSON.stringify(tableHelper.getWhere()));
-    // console.log(tableHelper.getOrder());
-    // console.log(tableHelper.getOffset());
-    // console.log(tableHelper.getLimit());
 
     userObj = tableHelper.getUser();
 
