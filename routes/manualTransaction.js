b59bf3ae4eede7514f6bfbddf763e30917494eda
sifan <sifansetu@gmail.com>
2019-01-16 04:43:31 +0600

    transaction details api created

--- routes/manualTransaction.js
+++ routes/manualTransaction.js
@@ -4,11 +4,17 @@ var multer = require("multer");
 var upload = multer();
 var DB = require("../models/index");
 var sequelize = DB.sequelize;
+var branchUtils = require("./../utils/branch");
 var manualTransaction = sequelize.models.manualTransactions;
 var passport = require('passport');
 var bodyParser = require('body-parser');
 var moment = require("moment-timezone");
 var timezoneConfig = require("../config/timezone");
+var bundleModel = sequelize.models.bundle;
+var HttpStatus = require("http-status-codes");
+var Promise = require("bluebird");
+var _ = require("underscore");
+
 
 router.use(bodyParser.json()); // for parsing application/json
 router.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
@@ -31,9 +37,9 @@ router.post("/create", upload.array(), function (req, res) {
     postData['payment_reference'] = req.body.payment_reference;
     postData['payment_description'] = req.body.payment_description;
     postData['created_by'] = req.user.email;
-    postData['source_branch_id']=req.body.source_branch_id;
-    postData['source_branch_type']=req.body.source_branch_type;
-    postData['instructed_by']=req.body.instructed_by;
+    postData['source_branch_id'] = req.body.source_branch_id;
+    postData['source_branch_type'] = req.body.source_branch_type;
+    postData['instructed_by'] = req.body.instructed_by;
 
 
     sequelize.transaction(function (t) {
@@ -65,6 +71,35 @@ router.put("/receivetransaction/:id", function (req, res) {
     ).then(function (result) {
         res.status(200);
         res.send({status: "success", data: result})
+    }).catch(function (err) {
+        res.status(500);
+        res.send({status: "error", data: null, message: err})
+    });
+
+})
+var manualtransactionDetails = {};
+var transaction_details = null;
+router.get("/details/:id", function (req, res) {
+
+
+    return manualTransaction.findOne({
+        where: {
+            id: req.params.id
+        }
+    }).then(function (transactionDetails) {
+        transaction_details = transactionDetails;
+        return branchUtils.getInclusiveBranchInstance(transactionDetails.source_branch_type, transactionDetails.source_branch_id);
+    }).then(function (branchinfo) {
+        manualtransactionDetails['created_by'] = transaction_details.created_by;
+        manualtransactionDetails['creation_date'] = transaction_details.createdAt;
+        manualtransactionDetails['instructed_by'] = transaction_details.instructed_by;
+        manualtransactionDetails["id"] = transaction_details.id;
+        manualtransactionDetails["branch_type"] = transaction_details.source_branch_type
+        manualtransactionDetails['branch_label'] = branchinfo.label;
+        manualtransactionDetails['description']=transaction_details.payment_description
+
+        res.send({data: manualtransactionDetails});
+
     }).catch(function (err) {
         res.status(500);
         res.send({status: "error", data: null, message: err})
