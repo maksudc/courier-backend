23550239efc0622967e8a0a352e2f42dda382221
sifan <sifansetu@gmail.com>
2019-01-18 04:17:06 +0600

    list with branch label done

--- routes/data_binder/manualTransaction/viewpendingCashin.js
+++ routes/data_binder/manualTransaction/viewpendingCashin.js
@@ -5,9 +5,15 @@ var DB = require("./../../../models/index");
 var cashinModel = DB.sequelize.models.manualTransactions;
 var DataTableHelper = require("./../../../utils/data_binder/dataTable");
 var panicUtils = require("./../../../utils/panic");
+var Promise = require("bluebird");
+var branchUtils = require("./../../../utils/branch");
+var moment = require("moment-timezone");
+var timezoneConfig = require("./../../../config/timezone");
+var _ = require("underscore");
 
 
 router.get('/', function (req, res) {
+
 
     tableHelper = new DataTableHelper(req.query);
 
@@ -50,12 +56,38 @@ router.get('/', function (req, res) {
         .then(function (orderList) {
 
             resultData["data"] = orderList;
-            resultData["recordsTotal"] = orderList.count;
-            resultData["recordsFiltered"] = orderList.count;
 
+            return Promise.resolve(orderList.rows);
             res.status(HttpStatus.OK);
-            res.send(resultData);
-        })
+            //  res.send(resultData);
+        }).map(function (itemInstance) {
+
+        itemMap = {
+            "id": itemInstance.id,
+            "createdAt": moment.tz(itemInstance.createdAt, timezoneConfig.COMMON_ZONE).tz(timezoneConfig.CLIENT_ZONE).format("YYYY-MM-DD HH:mm:ss"),
+            "created_by": itemInstance.created_by,
+            "instructed_by": itemInstance.instructed_by,
+
+        };
+
+        return Promise.all([
+            Promise.resolve(itemMap),
+            branchUtils.getInclusiveBranchInstance(itemInstance.source_branch_type, itemInstance.source_branch_id),
+
+        ]);
+    }).map(function (resultwithbranch) {
+
+        itemMap = resultwithbranch[0];
+        branchInstance = resultwithbranch[1];
+        itemMap["source_branch_type"] = branchInstance.label;
+
+        return Promise.resolve(itemMap);
+    }).then(function (itemMaps) {
+        resultData["data"]["rows"] = getSortedItemMaps(itemMaps);
+
+        res.status(HttpStatus.OK);
+        res.send(resultData);
+    })
         .catch(function (err) {
             if (err) {
                 console.error(err.stack);
@@ -65,5 +97,39 @@ router.get('/', function (req, res) {
         });
 
 });
+
+function getSortedItemMaps(itemMaps) {
+
+    sortedItemMaps = [];
+
+    orderCodeSet = new Set();
+    for (I = 0; I < itemMaps.length; I++) {
+        orderCodeSet.add(parseInt(itemMaps[I].id));
+    }
+
+    orderCodeArray = Array.from(orderCodeSet);
+    orderCodeArray.sort(function (a, b) {
+        return a - b;
+    });
+
+    itemsSortedByOrderCode = _.sortBy(itemMaps, "id");
+    itemsGroupedByOrder = _.groupBy(itemsSortedByOrderCode, "id");
+
+    var sortedItemMaps = [];
+
+    for (I = 0; I < orderCodeArray.length; I++) {
+
+        orderCode = orderCodeArray[I];
+
+        singleOrderSortedItems = _.sortBy(itemsGroupedByOrder[orderCode], function (itemDescriptor) {
+            item_code = itemDescriptor["id"];
+            return parseInt(item_code);
+        });
+
+        sortedItemMaps = sortedItemMaps.concat(singleOrderSortedItems);
+    }
+
+    return sortedItemMaps;
+}
 
 module.exports = router;
