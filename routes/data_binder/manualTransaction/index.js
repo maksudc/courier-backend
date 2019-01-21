cdb53a475e9c9347b0aa357c6e18a116a7168397
sifan <sifansetu@gmail.com>
2019-01-21 20:46:24 +0600

    pending and received cashout feature added

--- routes/data_binder/manualTransaction/index.js
+++ routes/data_binder/manualTransaction/index.js
@@ -8,6 +8,12 @@ router.use('/pendingcashin/view', pendingcashinDataBinder);
 var receivedcashinDataBinder = require("./viewreceivedCashin");
 router.use('/receivedcashin/view', receivedcashinDataBinder);
 
+var pendingcashoutDataBinder = require("./viewpendingCashout");
+router.use('/pendingcashout/view', pendingcashoutDataBinder);
+
+var receivedcashoutDataBinder = require("./viewreceivedCashout");
+router.use('/receivedcashout/view', receivedcashoutDataBinder);
+
 var cashoutDataBinder = require("./viewCashout");
 router.use('/cashout/view', cashoutDataBinder);
 
