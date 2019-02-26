27d52ba6354ab2023c3805a3b3b58ac110780d88
sifan <sifansetu@gmail.com>
2019-02-27 01:43:12 +0600

    details api changed

--- routes/manualTransaction.js
+++ routes/manualTransaction.js
@@ -125,11 +125,11 @@ router.get("/details/:id", function (req, res) {
     }).then(function (transactionDetails) {
         transaction_details = transactionDetails;
         return branchUtils.getInclusiveBranchInstance(transactionDetails.source_branch_type, transactionDetails.source_branch_id);
-    }).then(function (source_branchinfo) {
-        source_branch_info = source_branchinfo;
+    }).then(function (sourcebranchinfo) {
+        source_branch_info =sourcebranchinfo;
         return branchUtils.getInclusiveBranchInstance(transaction_details.branch_type, transaction_details.branch_id)
 
-    }).then(function (target_branchinfo) {
+    }).then(function (target_branch_info) {
         manualtransactionDetails['created_by'] = transaction_details.created_by;
         manualtransactionDetails['creation_date'] = transaction_details.createdAt;
         manualtransactionDetails['instructed_by'] = transaction_details.instructed_by;
@@ -137,8 +137,8 @@ router.get("/details/:id", function (req, res) {
         manualtransactionDetails["id"] = transaction_details.id;
         manualtransactionDetails["source_branch_type"] = transaction_details.source_branch_type;
         manualtransactionDetails["target_branch_type"] = transaction_details.branch_type
-        manualtransactionDetails['source_branch_label'] = source_branch_info.label;
-        manualtransactionDetails['target_branch_info'] = target_branchinfo;
+        manualtransactionDetails['source_branch_info'] = source_branch_info;
+        manualtransactionDetails['target_branch_info'] = target_branch_info;
         manualtransactionDetails['description'] = transaction_details.payment_description;
         manualtransactionDetails['target_branch_id'] = transaction_details.branch_id;
         manualtransactionDetails['received_by'] = transaction_details.received_by;
