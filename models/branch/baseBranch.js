bd733ba843ab4a5444be42614214f6fecbdcdbfe
sifan <sifansetu@gmail.com>
2019-04-01 04:44:19 +0600

    some branches cannot handle vd

--- models/branch/baseBranch.js
+++ models/branch/baseBranch.js
@@ -5,6 +5,7 @@ BaseBranchModel = {
     label: Sequelize.STRING,
     phone: Sequelize.STRING,
     address: Sequelize.STRING,
+    vd_disabled: Sequelize.BOOLEAN,
     branchType: Sequelize.STRING,
     position: Sequelize.GEOMETRY,
 
