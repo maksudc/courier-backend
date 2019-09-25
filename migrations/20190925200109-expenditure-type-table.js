6af5e1593e66ddedf91f3a0f182cf39868f33330
sifan <sifansetu@gmail.com>
2019-09-26 02:12:15 +0600

    [wip] expenditure type table added

---
+++ migrations/20190925200109-expenditure-type-table.js
@@ -0,0 +1,56 @@
+'use strict';
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+
+        return queryInterface.createTable('expenditureType', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            expenditureType:
+                {
+                    type: Sequelize.STRING,
+                    allowNull: false
+                },
+            createdBy:
+                {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                },
+            updatedBy: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+
+        })
+        /*
+          Add altering commands here.
+          Return a promise to correctly handle asynchronicity.
+
+          Example:
+          return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+        */
+    },
+
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('expenditureType');
+        /*
+          Add reverting commands here.
+          Return a promise to correctly handle asynchronicity.
+
+          Example:
+          return queryInterface.dropTable('users');
+        */
+    }
+};
